b5118450a8c71026db80f060ebb857efba82fe0d
Elliot Forbes <emforce@live.com>
2018-06-10 08:37:42 +0100

    Added go-websocket-tutorial code

---
+++ go-websocket-tutorial/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"html"
+	"log"
+	"net/http"
+
+	socketio "github.com/googollee/go-socket.io"
+)
+
+func main() {
+	server, err := socketio.NewServer(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	server.On("connection", func(so socketio.Socket) {
+		log.Println("on connection")
+		so.Join("chat")
+		so.On("chat message", func(msg string) {
+			log.Println("emit:", so.Emit("chat message", msg))
+			so.BroadcastTo("chat", "chat message", msg)
+		})
+		so.On("disconnection", func() {
+			log.Println("on disconnect")
+		})
+	})
+	server.On("error", func(so socketio.Socket, err error) {
+		log.Println("error:", err)
+	})
+
+	http.Handle("/socket.io/", server)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+
+	log.Println("Serving at localhost:5000...")
+	log.Fatal(http.ListenAndServe(":5000", nil))
+}
