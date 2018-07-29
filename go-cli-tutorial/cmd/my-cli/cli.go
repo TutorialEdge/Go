a719a16e1e2a40f090ec59c8930ef3e45784311b
Elliot Forbes <emforce@live.com>
2018-07-29 14:30:59 +0100

    Added Go CLI Tutorial source code

---
+++ go-cli-tutorial/cmd/my-cli/cli.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Website Lookup CLI"
+	app.Usage = "Let's you query IPs, CNAMEs, MX records and Name Servers!"
+
+	myFlags := []cli.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "tutorialedge.net",
+		},
+	}
+
+	app.Commands = []cli.Command{
+		{
+			Name:  "ns",
+			Usage: "Looks Up the NameServers for a Particular Host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ns, err := net.LookupNS(c.String("url"))
+				if err != nil {
+					return err
+				}
+				for i := 0; i < len(ns); i++ {
+					fmt.Println(ns[i].Host)
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "cname",
+			Usage: "Looks up the CNAME for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				cname, err := net.LookupCNAME(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				fmt.Println(cname)
+				return nil
+			},
+		},
+		{
+			Name:  "ip",
+			Usage: "Looks up the IP addresses for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				ip, err := net.LookupIP(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				for i := 0; i < len(ip); i++ {
+					fmt.Println(ip[i])
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "mx",
+			Usage: "Looks up the MX records for a particular host",
+			Flags: myFlags,
+			Action: func(c *cli.Context) error {
+				mx, err := net.LookupMX(c.String("host"))
+				if err != nil {
+					fmt.Println(err)
+				}
+				for i := 0; i < len(mx); i++ {
+					fmt.Println(mx[i].Host, mx[i].Pref)
+				}
+				return nil
+			},
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
